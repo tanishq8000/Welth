9ef95d2e4a9895abe8393579841bd70801c57496
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-06-01 19:24:47 +0530

    dummy transactions

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { getAccountWithTransactions } from "@/actions/accounts";
+import { notFound } from "next/navigation";
 
-const AccountsPage = () => {
-  return <div>AccountsPage</div>;
-};
+export default async function AccountPage({ params }) {
+  const accountData = await getAccountWithTransactions(params.id);
 
-export default AccountsPage;
+  if (!accountData) {
+    notFound();
+  }
+
+  const { transactions, ...account } = accountData;
+
+  return (
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+            Account
+          </p>
+        </div>
+
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
+      </div>
+
+      {/* Chart Section */}
+
+      {/* Transactions Table */}
+    </div>
+  );
+}
