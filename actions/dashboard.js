c1a55026ce7c9ea9e5e465aa48095ef8b2690192
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-06-01 16:48:28 +0530

    Accounts Section

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -11,6 +11,9 @@ const serializeTransaction = (obj) => {
   if (obj.balance) {
     serialized.balance = obj.balance.toNumber();
   }
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
   return serialized;
 };
 
@@ -71,5 +74,39 @@ export async function createAccount(data) {
     return { success: true, data: serializedAccount };
   } catch (error) {
     throw new Error(error.message);
+  }
+}
+
+export async function getUserAccounts() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  try {
+    const accounts = await db.account.findMany({
+      where: { userId: user.id },
+      orderBy: { createdAt: "desc" },
+      include: {
+        _count: {
+          select: {
+            transactions: true,
+          },
+        },
+      },
+    });
+
+    // Serialize accounts before sending to client
+    const serializedAccounts = accounts.map(serializeTransaction);
+
+    return serializedAccounts;
+  } catch (error) {
+    console.error(error.message);
   }
 }
