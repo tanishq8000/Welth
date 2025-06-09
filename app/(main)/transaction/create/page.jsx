6ebd067011429eb23799a72636704c5f7c781c39
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-06-09 15:45:50 +0530

    Transaction Form

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { getUserAccounts } from "@/actions/dashboard";
+import { AddTransactionForm } from "../_components/transaction-form";
+import { defaultCategories } from "@/Data/categories";
 
-const AddTransactionPage = async () => {
-  return <div>AddTransactionPage</div>;
-};
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccounts();
 
-export default AddTransactionPage;
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+    </div>
+  );
+}
